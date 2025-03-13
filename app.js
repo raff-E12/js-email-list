7b36ae7de278c898f8f51ae7b80dead479c240c6
Raff_12 <raffelecoppola381@gmail.com>
2025-03-13 18:43:26 +0100

    feat: Conclusione della prima parte del progetto

---
+++ app.js
@@ -0,0 +1,41 @@
+// Scopo del progetto:
+// Creare un sito con le varie caselle di email varie con la seguente Api.
+
+// Creazione delle risorse generali
+const url = "https://flynn.boolean.careers/exercises/api/random/mail";
+let parent_node = document.getElementById("parent-node");
+
+//Creazione della seguente funzione di integrazione Api
+
+function HandleEmailGenBox(){
+    setTimeout(()=>{
+     let response = fetch(url).then(response => response.json()).then(data => HandleBoxEmailGen(data));
+     return response
+    }, 1200)
+}
+
+//Generazione degli elementi nel DOM
+const HandleBoxEmailGen = (data)=>{
+    setTimeout(()=>{
+       for (let index = 1; index < 10; index++) {
+        let create_box = document.createElement("div");
+        let fragment_parent = document.createDocumentFragment();
+        let text_title = document.createElement("h2");
+        let text_email = document.createElement("p");
+        create_box.className = "email-box";
+        let text_em = document.createTextNode(data.response);
+        let text_til = document.createTextNode("Email dell utente in questione:");
+    
+        text_email.appendChild(text_em);
+        text_title.appendChild(text_til)
+        create_box.append(text_title);
+        create_box.append(text_email);
+        fragment_parent.append(create_box);
+        parent_node.append(fragment_parent);
+       }
+    }, 1200)
+}
+
+window.addEventListener('load', ()=>{
+    HandleEmailGenBox();
+})
